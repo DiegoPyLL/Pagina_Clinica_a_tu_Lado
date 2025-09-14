e8cf38a20a7c2df3828eb0ed44ade2d01dfa470b
BRomanman <BA.ROMANF@DUOCUC.CL>
2025-09-14 00:37:19 -0300

    perfil de ususaarioy

---
+++ Paginas/perfil_paciente.js
@@ -0,0 +1,11 @@
+// pagina por si necesitamos agregar codigo al perfil del paciente
+document.getElementById('perfilPacienteForm').addEventListener('submit', function(event) {
+    // Evita que el formulario se envíe de la forma tradicional
+    event.preventDefault();
+
+    // Aquí puedes agregar la lógica para guardar los cambios (por ejemplo, con fetch())
+    console.log('Guardando cambios...');
+
+    // Después de guardar, redirige a la nueva página
+    window.location.href = "p_Principal.html";
+});
